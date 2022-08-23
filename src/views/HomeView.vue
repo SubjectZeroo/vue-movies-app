52db80412f1888476c95db3c92a45763e749c449
Jafeht Soturno <jasordev@gmail.com>
2022-08-22 22:47:13 -0400

    cambiandi api de omdb a tmdb

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,8 +9,8 @@
     <div class="popular-movies">
       <div class="uppercase tracking-wider text-orange-500 text-lg font-semibold">
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
-          <div class="mt-8"  v-for="movie in movies" :key="movie.imdbID">
-           <img :src="movie.Poster" alt="Movie Poster">
+          <div class="mt-8" v-for="movie in movies" :key="movie.id">
+           <img :src="'https://image.tmdb.org/t/p/w500'+movie.poster_path" :alt="movie.original_title">
           </div>
         </div>
       </div>
@@ -23,11 +23,12 @@ export default {
     setup() {
         const movies = ref([]);
         fetch(
-          `http://www.omdbapi.com/?i=tt3896198&apikey=427cab4e&s=joker`
+          `http://api.themoviedb.org/3/movie/popular?api_key=aa5ed91c2023f45e0f1d530c535f41ec`
         )
           .then((response) => response.json())
           .then((data) => {
-            movies.value = data.Search;
+            console.log(data.results);
+            movies.value = data.results;
           });
      
   
