d6975b3e5774bd1ad9cbb0a405564d7fac690eda
Jafeht Soturno <jasordev@gmail.com>
2022-08-24 22:53:11 -0400

    agregando components navbar y series, actors solo los archivos

---
+++ src/components/MovieList.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container mx-auto px-4 pt-16">
+    <div class="popular-movies">
+      <div class="uppercase tracking-wider text-orange-500 text-lg font-semibold">
+        Popular Movies
+       </div>
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+          <div class="mt-8" v-for="movie in movies" :key="movie.id">
+            <a href="">
+                <img :src="'https://image.tmdb.org/t/p/w500'+movie.poster_path" :alt="movie.original_title">
+            </a>
+            <div class="mt-2">
+                <a href="" class="text-lg mt-2 hover:text-gray-300">
+                {{ movie.title }}
+                </a>
+                <div class="flex items-center text-gray-400 text-sm mt-1">
+                  <svg class="fill-current text-orange-500 w-4" viewBox="0 0 24 24"><g data-name="Layer 2"><path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star"></path></g></svg>
+                  <span class="ml-1">{{ movie.vote_average * 10}}%</span>
+                  <span class="mx-2">|</span>
+                  <span>{{ movie.release_date }}</span>
+                </div>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+ setup() {
+        const movies = ref([]);
+        fetch(
+          `http://api.themoviedb.org/3/movie/popular?api_key=aa5ed91c2023f45e0f1d530c535f41ec`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            movies.value = data.results;
+          });
+     
+  
+      return {
+          movies,  
+      };
+    },
+}
+</script>
+
+<style>
+
+</style>
